2a60c15758986d3e1e810b01974f329f87da6b6e
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2022-09-18 23:46:33 -0500

    Configuracion typeorm producto

--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProductoEntity } from './producto.entity';
 import { ProductoService } from './producto.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductoEntity])],
   providers: [ProductoService]
 })
 export class ProductoModule {}
