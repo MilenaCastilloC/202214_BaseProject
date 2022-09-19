92747a07389f2ee174db261fbcbc2e9d5cf5ac66
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2022-09-18 23:52:36 -0500

    Configuracion service spect producto

--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { BusinessLogicException, BusinessError } from '../shared/errors/business-errors'
+import { ProductoEntity } from './producto.entity';
+
 
 @Injectable()
-export class ProductoService {}
+export class ProductoService {
+    constructor(
+        @InjectRepository(ProductoEntity)
+        private readonly museumRepository: Repository<ProductoEntity>
+    ){}
+}
