986cba47ad02518b988bdd8e50f70a075edfd326
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2022-09-18 20:44:31 -0500

    Creación de atributos Producto

--- src/producto/producto.entity.ts
+++ src/producto/producto.entity.ts
@@ -1,1 +1,18 @@
-export class ProductoEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity
+export class ProductoEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+ 
+    @Column()
+    precio: number;
+ 
+    @Column()
+    tipo: string;
+
+}
+
