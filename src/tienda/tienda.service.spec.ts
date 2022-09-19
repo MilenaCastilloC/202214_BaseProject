e4351bc7e8acae818c4699ed8e10aa2c15858dac
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2022-09-18 22:38:58 -0500

    Configuracion pruebas tienda

--- src/tienda/tienda.service.spec.ts
+++ src/tienda/tienda.service.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TiendaService } from './tienda.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { TiendaEntity } from './tienda.entity';
+import { TiendaService } from './tienda.service'
 
 describe('TiendaService', () => {
-  let service: TiendaService;
+ let service: TiendaService;
+ let repository: Repository<TiendaEntity>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TiendaService],
-    }).compile();
+ beforeEach(async () => {
+   const module: TestingModule = await Test.createTestingModule({
+     imports: [...TypeOrmTestingConfig()],
+     providers: [TiendaService],
+   }).compile();
 
-    service = module.get<TiendaService>(TiendaService);
-  });
+   service = module.get<TiendaService>(TiendaService);
+   repository = module.get<Repository<TiendaEntity>>(getRepositoryToken(TiendaEntity));
+ });
+  
+ it('should be defined', () => {
+   expect(service).toBeDefined();
+ });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
 });
