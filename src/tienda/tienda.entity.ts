8da538b18091dc16e88ee553d536de1f7e875387
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2022-09-18 20:56:25 -0500

    Asiganción de atributos para la clase tienda

--- src/tienda/tienda.entity.ts
+++ src/tienda/tienda.entity.ts
@@ -1,1 +1,16 @@
-export class TiendaEntity {}
+import { Column, PrimaryGeneratedColumn, Entity } from "typeorm";
+
+@Entity()
+export class TiendaEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+ 
+    @Column()
+    ciudad: number;
+ 
+    @Column()
+    direccion: string;
+}
