a2af5efcf3a8f8c84d6a1e4317c174e87065a778
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2022-09-18 21:52:26 -0500

    Definicion del constructor

--- src/tienda/tienda.service.ts
+++ src/tienda/tienda.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { TiendaEntity } from './tienda.entity';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+ 
 @Injectable()
-export class TiendaService {}
+export class TiendaService {
+    constructor(
+        @InjectRepository(TiendaEntity)
+        private readonly tiendaRepository: Repository<TiendaEntity>
+    ){}
+}
